998cc62cac1f695e54eea5e64724ac1ce9d27c51
Hun <causeand@naver.com>
2017-01-05 14:14:05 +0900

    Tutrial StoryTeller class Editor Coding

---
+++ 2016-D/Assets/Scripts/StoryTeller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryTeller : MonoBehaviour {
+
+
+	public enum ScriptType { Text_I, Text_Catcher, Question_Catcher, Null };
+
+	[SerializeField]
+	private List<string> Script;
+	[SerializeField]
+	private List<float> Time;
+	[SerializeField]
+	private List<ScriptType> Type;
+
+
+	[SerializeField]
+	List<bool> MakeCondition = new List<bool>();
+	[SerializeField]
+	private List<string> ConditionScript;
+	[SerializeField]
+	private List<int> ConditionIndexStart;
+	[SerializeField]
+	private List<int> ConditionIndexEnd;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
